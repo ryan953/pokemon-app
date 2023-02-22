5f16a45de88d97d2e46d1f760155c2157e0e9e51
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-02-22 14:11:45 -0800

    upgrade replay sdk and make better dsn configs

--- src/index.js
+++ src/index.js
@@ -3,39 +3,34 @@ import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
 import {store} from './redux';
-
 import App from './App';
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
-import { SentryReplay } from '@sentry/replay';
+
+const DSNs = {
+  'pokedex@wildcard-inc': "https://32e900439c044907a850b9bae30864f9@o1170741.ingest.sentry.io/6731936",
+};
 
 Sentry.init({
-  // pokedex @ wildcard-inc
-  dsn: "https://32e900439c044907a850b9bae30864f9@o1170741.ingest.sentry.io/6731936",
-  // pokedex @ local devserver
-  // dsn: 'http://7fb78fda3b124ed48f7f88b0ad215de8@127.0.0.1:8000/2',
+  dsn: DSNs["pokedex@wildcard-inc"],
 
   release: process.env.REACT_APP_SENTRY_RELEASE,
-  integrations: [
-    new BrowserTracing(),
-    new SentryReplay({
-      stickySession: true, // Default is true
-      recordingConfig: {
-        maskAllInputs: true, // Default is true
-        blockClass: 'sr-block',
-        maskTextClass: 'sr-mask',
-        // maskTextSelector: '*', // Hides all text on the page
-        // blockSelector: 'img', // Hide all <img> tags
-      },
-    }),
-  ],
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
-});
 
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 1.0,
+
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  integrations: [new BrowserTracing(), new Sentry.Replay()],
+});
 
 ReactDOM.render(
     <Provider store={store}>
